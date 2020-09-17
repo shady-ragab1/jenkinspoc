5f40f224680871846bb29390af66c9c457f2410e
Shady Ragab <eng.shady.ragab.1@gmail.com>
2020-09-17 20:35:18 +0200

    Initial commit

---
+++ src/main/java/com/shady/jenkinspoc/JenkinspocApplication.java
@@ -0,0 +1,13 @@
+package com.shady.jenkinspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinspocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinspocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shady/jenkinspoc/JenkinspocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shady.jenkinspoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinspocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
